6dafda13f802e266e01185538333ad4dacc4194d
Jahdiel <jahdielnoboh1@gmail.com>
2026-05-23 18:49:29 +0100

    Location page adjustment and responsiveness

--- src/Components/AboutPages/Loc.jsx
+++ src/Components/AboutPages/Loc.jsx
@@ -16,31 +16,29 @@ export default function Loc() {
                 alt="Collage" 
                 className={styles.img3} 
                 />
-                <span className={styles.txtcollage}>
+                <div className={styles.txtcollage}>
                 <p className={styles.locitem}>
                     <span className={styles.icon}>📍</span>
                         <span>
-                        12 Aminu Kano Crescent<br/>
-                        Suite 5B<br/>
-                        Wuse II, Abuja 900288
+                        N-132nd Street<br/>
+                        W Maple Road<br/>
+                        Wuse II, Abuja 900288 <br />
+                        <span className={styles.locsub}>Opposite Target</span>
                         </span>
-                            </p>
-                <p className={styles.locsub}>
-                    Opposite Banex Plaza
-                </p>
+                    </p>
             
             <p className={styles.locitem}>
                 <span className={styles.icon}>📞</span>
-                <a href="tel:+2349059278608"> (090) 592-78608</a>
+                <a href="tel:+2349059278608">0905-927-8608</a>
                 </p>
                 <p className={styles.loctitle}>Store hours</p>
 
-            <p className={`${styles.lochours} ${styles.p}`}>
-                Mon - Fri: 9:00 am - 6:30 pm;<br/>
-                Sat: 10:00 am - 7:30 pm;<br/>
-                Sun: 12:00 pm - 5:00 pm;
-            </p>
-                    </span>
+                <p className={styles.locitem}>
+                    Mon - Fri: 9:00 am - 6:30 pm;<br/>
+                    Sat: 10:00 am - 7:30 pm;<br/>
+                    Sun: 12:00 pm - 5:00 pm;
+                </p>
+                </div>
                 </div>
         </section>
   );
